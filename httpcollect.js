904201157b433ef48350bce50dee541d649ae8ac
David Tsao <davidt1154@gmail.com>
2014-10-15 22:46:55 -0400

    finished http collect

---
+++ httpcollect.js
@@ -0,0 +1,16 @@
+/*globals require, process, console */
+var http = require('http');
+var bl = require('bl');
+var url = process.argv[2];
+
+http.get(url, function (response) {
+	'use strict';
+	response.pipe(bl(function (err, data) {
+		if (err) {
+			return console.error(err);
+		}
+		data = data.toString();
+		console.log(data.length);
+		console.log(data);
+	}));
+});
